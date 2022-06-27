dbecfedf536bc5332b1a1a05bc99e5e5ecfa8121
nicovelar <nicovelar@gmail.com>
2022-06-27 12:50:33 -0300

    Movimiento de la Pelota

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 var game = function () {
     //Declaración de variables
-    let time = 15;
+    let time = 20;
     let movement = 10;
     let movementBar = 20;
     let width = document.documentElement.clientWidth - movement;
@@ -30,9 +30,52 @@ var game = function () {
 
     //Pone a funcionar el juego frame a frame según el intérvalo
     function play() {
+        moveBall()
         moveBar()
     }
 
+    //Selecciona uno de los 4 casos y los mueve según el caso
+    function moveBall() {
+        checkStateBall();
+        switch(ball.state){
+            case 1: // derecha, abajo
+                ball.style.left = (ball.offsetLeft + movement) +"px";
+                ball.style.top = (ball.offsetTop + movement) +"px";
+                break;
+            case 2: // derecha, arriba
+                ball.style.left = (ball.offsetLeft + movement) +"px";
+                ball.style.top = (ball.offsetTop - movement) +"px";
+                break;
+            case 3: // izquierda, abajo
+                ball.style.left = (ball.offsetLeft - movement) +"px";
+                ball.style.top = (ball.offsetTop + movement) +"px";
+                break;
+            case 4: // izquierda, arriba
+                ball.style.left = (ball.offsetLeft - movement) +"px";
+                ball.style.top = (ball.offsetTop - movement) +"px";
+                break;
+        }
+    }
+
+    function checkStateBall() {
+        if(ball.direction === 1) {
+            if(ball.offsetTop >= height) ball.state = 2;
+            else if(ball.offsetTop <=0) ball.state =1;
+        }else {
+            if(ball.offsetTop >= height) ball.state = 4;
+            else if(ball.offsetTop <=0) ball.state =3;
+        }
+    }
+
+    function collidePlayer1() {
+        return false;
+    }
+
+    function collidePlayer2() {
+        return false;
+    }
+
+    //Mueve las barras según la tecla presionada
     function moveBar() {
         if (player1.keyPress) {
             if(player1.key == "w" && barLeft.offsetTop >= 10)
