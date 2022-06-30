604b810595edbd3bc2b7f8c1251e3c305fb13f68
nicovelar <nicovelar@gmail.com>
2022-06-30 14:43:09 -0300

    Updates

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname + "./public"));
+app.use(express.static(__dirname + "/public"));
 
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, './index.html'));
+    res.sendFile(path.join(__dirname, '/index.html'));
   });
 
 app.listen(port, () => {
